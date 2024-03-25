36ad48c0059db14732e994a6da156136272a3b20
etsun4 <esun4@ocdsb.ca>
2024-03-25 10:10:42 -0400

    login should be good

--- Project/jsFiles/login.js
+++ Project/jsFiles/login.js
@@ -13,9 +13,6 @@ firebase.initializeApp(firebaseConfig);
 var chess4allDB = firebase.database().ref('chess4all');
 const auth = firebase.auth()
 
-
-
-
 document.getElementById("user_login").addEventListener("submit", submitForm);
 
 function submitForm(e) {
