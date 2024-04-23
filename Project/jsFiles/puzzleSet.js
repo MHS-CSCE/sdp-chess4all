2273fd910de6e3f3f6325408656000815702103e
chessdudeo7 <142058575+chessdudeo7@users.noreply.github.com>
2024-04-23 17:21:37 -0400

    Creating function to load new puzzles onclick of a button

--- Project/jsFiles/puzzleSet.js
+++ Project/jsFiles/puzzleSet.js
@@ -1,20 +1,37 @@
-const array = [["Brook", "Bknight", "Bbishop", "Bqueen", "Bking", "Bbishop", "Bknight", "Brook"], ["Bpawn", "Bpawn", "Bpawn", "Bpawn", "Bpawn", "Bpawn", "Bpawn", "Bpawn"], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["Wpawn", "Wpawn", "Wpawn", "Wpawn", "Wpawn", "Wpawn", "Wpawn", "Wpawn"], ["Wrook", "Wknight", "Wbishop", "Wqueen", "Wking", "Wbishop", "Wknight", "Wrook"]]
+const array0 = [["Brook", "Bknight", "Bbishop", "Bqueen", "Bking", "Bbishop", "Bknight", "Brook"], ["Bpawn", "Bpawn", "Bpawn", "Bpawn", "Bpawn", "Bpawn", "Bpawn", "Bpawn"], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["Wpawn", "Wpawn", "Wpawn", "Wpawn", "Wpawn", "Wpawn", "Wpawn", "Wpawn"], ["Wrook", "Wknight", "Wbishop", "Wqueen", "Wking", "Wbishop", "Wknight", "Wrook"]]
+
+const array1 = [["Brook", "Brook", "Brook", "Bqueen", "Bking", "Brook", "Brook", "Brook"], ["Brook", "Brook", "Brook", "Brook", "Brook", "Brook", "Brook", "Brook"], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", ""], ["Wrook", "Wrook", "Wrook", "Wrook", "Wrook", "Wrook", "Wrook", "Wrook"], ["Wrook", "Wrook", "Wrook", "Wqueen", "Wking", "Wrook", "Wrook", "Wrook"]]
 
 var piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8
 
+puzzlearray = [array0, array1]
 piecearray = [piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8]
 
-for (i = 0; i < 8; i++) {
-    for (j = 0; j < 8; j++) {
-        piecearray[j] = array[i][j]
+var counter = 0
+
+function insertPieces() {
+    console.log(counter)
+    for (i = 0; i < 8; i++) {
+        row = 10*(8-i)
+        console.log(row)
+        for (j = 0; j < 8; j++) {
+            piecearray[j] = puzzlearray[counter][i][j]
+            console.log(piecearray[j])
+            document.getElementById("b" + String(row) + String(j+1)).innerHTML = piecearray[j]
+            console.log("b" + String(row) + String(j+1))
+        }
     }
-    insertPieces(i, piecearray)
+    insertImage()
+    counter++
 }
 
-function insertPieces(i, piecearray) {
-    row = 10*(8-i)
-    for (x = 1; x < 9; x++) {
-        document.getElementById("b" + String(row) + String(x)).innerHTML = piecearray[x-1]
-    }
-}
+//Completing Puzzle
 
+
+/*if(document.getElementById("b205").innerText == "" && document.getElementById("b405").innerText == "Wpawn"){
+    alert("correct");
+}
+else{
+    alert("incorrect");
+}
+*/
