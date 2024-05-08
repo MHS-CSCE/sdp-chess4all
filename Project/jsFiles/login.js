56a93af76cedda512a26475e67f202c67713435c
etsun4 <esun4@ocdsb.ca>
2024-05-08 08:33:16 -0400

    added some documentation for signup and login

--- Project/jsFiles/login.js
+++ Project/jsFiles/login.js
@@ -7,30 +7,36 @@ const firebaseConfig = {
     messagingSenderId: "600603932859",
     appId: "1:600603932859:web:b6606cc0ef9bb70504d19e"
 };
-
+//initialize the firebase database
 firebase.initializeApp(firebaseConfig);
-
+//variable for the firebase authentication function (built in)
 const auth = firebase.auth()
 
-
+//runs function when the submit button is clicked on the login page
 document.getElementById("user_login").addEventListener("submit", submitForm);
 
 function submitForm(e) {
+    //prevents the default submitting of the form
     e.preventDefault();
-
+    //creates variables for the email and password taking it form the values entered
     var email = document.getElementById('email').value;
     var password = document.getElementById('psw').value;
-
+    /**
+     * passes the email and password to the database and checks if there is an existing account
+     * if the account is already registered the user will be brought to the main page
+     * if not there will be an alert saying that their information is incorrect
+     * @param {string} email the email entered by the user
+     * @param {string} password the password entered by the user
+     */
     auth.signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
         var user = userCredential.user;
-        console.log("User logged in:", user);
         window.location.href = "/html/main.html";
     })
     .catch((error) => {
         document.querySelector(".alert").innerHTML = "Incorrect email or password";
         document.querySelector(".alert").style.display = "block";
     });
-
+    //resets the form
     document.getElementById("user_login").reset();
 }
--- Project/jsFiles/signup.js
+++ Project/jsFiles/signup.js
@@ -7,42 +7,50 @@ const firebaseConfig = {
     messagingSenderId: "600603932859",
     appId: "1:600603932859:web:b6606cc0ef9bb70504d19e"
 };
-
+//initializes the firebase database
 firebase.initializeApp(firebaseConfig);
-
+//variable for the authentication function
 const auth = firebase.auth()
-
+//event listener to know when form is submitted and runs submitForm function
 document.getElementById("chess4all").addEventListener("submit", submitForm);
 
 function submitForm(e) {
+    //prevents the default submitting of the form
     e.preventDefault();
-
+    //saves the name, email and password entered by the user, however the name is not used yet
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('psw').value;
-
+    //correct format for email
     var valid_email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //checks for a valid email in the correct format example@something.com (baed on the valid_email var)
     if (!valid_email.test(email)) {
         document.querySelector(".alert").innerHTML = "Email adress invalid";
         document.querySelector(".alert").style.display = "block";
         return;
     }
-
+    //checks the length of the password
     if (password.length < 6) {
         document.querySelector(".alert").innerHTML = "Password must be at least 6 characters long.";
         document.querySelector(".alert").style.display = "block";
         return;
     }
-
+    /**
+     * creates the user using the createUserWithEmailAndPassword function that is built into firebase authentication
+     * (sends the information to the database where it's saved)
+     * @param {string} email the email entered by the user
+     * @param {string} password the password entered by the user
+     */
     auth.createUserWithEmailAndPassword(email, password)
 
     
-
+    //pause before continuing
     setTimeout(() => {
         document.querySelector(".alert").style.display = "none";
     }, 3000);
-
+    //resets the form
     document.getElementById("chess4all").reset()
+    //brings to home page
     window.location.href = "/html/main.html";
 
 }
